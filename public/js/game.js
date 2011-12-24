cacc2d3abcc498e0f3c670a98560538cc69ea92a
Kiba <hackerkiba@gmail.com>
2011-12-24 09:07:09 +0000

    update output

--- public/js/game.js
+++ public/js/game.js
@@ -1,5 +1,5 @@
 (function() {
-  var Map, mapDraw, menu;
+  var Map, Unit, mapDraw, menu, unitDraw;
   Map = (function() {
     function Map(width, height) {
       var h;
@@ -10,16 +10,48 @@
         }
       }
     }
+    Map.prototype.generate = function() {
+      var h, w, _ref, _results;
+      _results = [];
+      for (h = 0, _ref = this.map.length; 0 <= _ref ? h <= _ref : h >= _ref; 0 <= _ref ? h++ : h--) {
+        if (h < this.map.length) {
+          _results.push((function() {
+            var _ref2, _results2;
+            _results2 = [];
+            for (w = 0, _ref2 = this.map[h].length; 0 <= _ref2 ? w <= _ref2 : w >= _ref2; 0 <= _ref2 ? w++ : w--) {
+              if (w < this.map[h].length) {
+                _results2.push((Math.random() * 10) > 5 ? this.map[h][w] = 1 : this.map[h][w] = 0);
+              }
+            }
+            return _results2;
+          }).call(this));
+        }
+      }
+      return _results;
+    };
+    Map.prototype.result = function() {
+      return this.map;
+    };
     return Map;
+  })();
+  Unit = (function() {
+    function Unit(x, y, name, type) {
+      this.x = x;
+      this.y = y;
+      this.name = name;
+      this.type = type;
+      this.goal_x = this.x;
+      this.goal_y = this.y;
+    }
+    return Unit;
   })();
   mapDraw = (function() {
     function mapDraw(width, height) {
       this.width = width;
       this.height = height;
     }
-    mapDraw.prototype.draw = function(p5) {
+    mapDraw.prototype.draw = function(p5, map) {
       var height, width, _ref, _results;
-      p5.noFill();
       p5.stroke(255);
       _results = [];
       for (height = 0, _ref = this.height; 0 <= _ref ? height <= _ref : height >= _ref; 0 <= _ref ? height++ : height--) {
@@ -29,6 +61,11 @@
             _results2 = [];
             for (width = 0, _ref2 = this.width; 0 <= _ref2 ? width <= _ref2 : width >= _ref2; 0 <= _ref2 ? width++ : width--) {
               if (width < this.width) {
+                if (map[height][width] === 1) {
+                  p5.noFill();
+                } else {
+                  p5.fill();
+                }
                 _results2.push(p5.rect(20 * width, 20 * height, 20, 20));
               }
             }
@@ -40,16 +77,32 @@
     };
     return mapDraw;
   })();
+  unitDraw = (function() {
+    function unitDraw() {}
+    unitDraw.prototype.draw = function(p5, unit) {
+      p5.fill();
+      if (unit.type === 1) {
+        p5.fill(255, 69, 0);
+        return p5.text("H", unit.x * 20 + 5, unit.y * 20 - 5);
+      }
+    };
+    return unitDraw;
+  })();
   menu = function(p5) {
     p5.setup = function() {
       p5.size($(window).width(), $(window).height());
       p5.frameRate(50);
-      return p5.background(0);
+      p5.background(0);
+      this.map = new Map(100, 100);
+      this.map.generate();
+      return this.unit = new Unit(10, 10, "Miya", 1);
     };
     return p5.draw = function() {
-      var map;
-      map = new mapDraw(100, 100);
-      return map.draw(p5);
+      var map_draw, unit_draw;
+      map_draw = new mapDraw(100, 100);
+      map_draw.draw(p5, this.map.result());
+      unit_draw = new unitDraw();
+      return unit_draw.draw(p5, this.unit);
     };
   };
   $(document).ready(function() {
