5018eb84c4728a473176f29e8f1a853fbbfff1a5
Kiba <hackerkiba@gmail.com>
2011-12-24 18:06:26 +0000

    cleanup and configure

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,8 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "the-city"
+set :repository,  "git://github.com/kiba/the-city.git"
+set :deploy_to, "/home/kiba/projects/the-city"
+set :scm, :git
+set :use_sudo, false
+set :branch "master"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+server "the-city.kibabase.com", :app, :primary => true
