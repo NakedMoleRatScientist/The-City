812552589dabaad8c9b7fb6946e7cc11f9a8c7af
Kiba <hackerkiba@gmail.com>
2011-12-31 05:39:27 +0000

    update

--- public/js/game.js
+++ public/js/game.js
@@ -252,9 +252,9 @@
   camera_input = function(key, map) {
     console.log(key.code);
     if (key.code === 97) {
-      return map.move_camera(-1, 0);
-    } else if (key.code === 100) {
       return map.move_camera(1, 0);
+    } else if (key.code === 100) {
+      return map.move_camera(-1, 0);
     } else if (key.code === 115) {
       return map.move_camera(0, -1);
     } else if (key.code === 119) {
