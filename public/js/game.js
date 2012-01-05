32ec502edaf106bc2983332b9c1f0b283a865856
Kiba <hackerkiba@gmail.com>
2012-01-05 04:44:37 +0000

    update

--- public/js/game.js
+++ public/js/game.js
@@ -1,45 +1,76 @@
 (function() {
-  var Body, Map, Message, Part, Unit, Units, camera_input, human_body, mapDraw, menu, message_draw, unitDraw;
-  Map = (function() {
-    function Map(width, height) {
-      var h;
-      this.camera_x = 0;
-      this.camera_y = 0;
-      this.map = new Array(height);
-      for (h = 0; 0 <= height ? h <= height : h >= height; 0 <= height ? h++ : h--) {
-        if (h < height) {
-          this.map[h] = new Array(width);
-        }
-      }
+  var Body, GameMode, GameModeDraw, Map, MenuMode, MenuModeDraw, Message, Mode, ModeDraw, Part, Subpart, Torso, Unit, Units, camera_input, human_body, list, listDraw, mapDraw, menu, message_draw, titleDraw, unitDraw;
+  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+    for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+    function ctor() { this.constructor = child; }
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor;
+    child.__super__ = parent.prototype;
+    return child;
+  };
+  GameMode = (function() {
+    function GameMode() {
+      this.map = new Map(100, 100);
+      this.map.generate();
+      this.units = new Units();
+      this.units.units.push(new Unit(10, 10, "Miya", 1));
+      this.units.units.push(new Unit(10, 20, "John", 1));
+      this.units.units[1].hostility = 1;
+      this.units.units[0].target = this.units.units[1];
+      this.message = new Message();
     }
-    Map.prototype.generate = function() {
-      var h, w, _ref, _results;
-      _results = [];
-      for (h = 0, _ref = this.map.length; 0 <= _ref ? h <= _ref : h >= _ref; 0 <= _ref ? h++ : h--) {
-        if (h < this.map.length) {
-          _results.push((function() {
-            var _ref2, _results2;
-            _results2 = [];
-            for (w = 0, _ref2 = this.map[h].length; 0 <= _ref2 ? w <= _ref2 : w >= _ref2; 0 <= _ref2 ? w++ : w--) {
-              if (w < this.map[h].length) {
-                _results2.push((Math.random() * 10) > 5 ? this.map[h][w] = 1 : this.map[h][w] = 0);
-              }
-            }
-            return _results2;
-          }).call(this));
-        }
-      }
-      return _results;
+    GameMode.prototype.act = function() {
+      this.units.move();
+      this.message.update(this.units.units);
+      return this.units.clean();
     };
-    Map.prototype.result = function() {
-      return this.map;
-    };
-    Map.prototype.move_camera = function(x, y) {
-      this.camera_x += x;
-      return this.camera_y += y;
-    };
-    return Map;
+    return GameMode;
   })();
+  GameModeDraw = (function() {
+    function GameModeDraw(p5) {
+      this.p5 = p5;
+      this.unit_draw = new unitDraw(p5, this.units, this.map);
+      this.map_draw = new mapDraw(100, 100);
+    }
+    GameModeDraw.prototype.draw = function() {
+      this.p5.background(0);
+      this.map_draw.draw(this.p5);
+      return this.unit_draw.draw();
+    };
+    return GameModeDraw;
+  })();
+  list = function() {
+    return [new GameMode(), new MenuMode()];
+  };
+  listDraw = function(p5) {
+    return [new GameModeDraw(p5), new MenuModeDraw(p5)];
+  };
+  MenuMode = (function() {
+    function MenuMode() {}
+    MenuMode.prototype.act = function() {};
+    return MenuMode;
+  })();
+  MenuModeDraw = (function() {
+    function MenuModeDraw(p5) {
+      this.p5 = p5;
+    }
+    MenuModeDraw.prototype.draw = function() {
+      return titleDraw(this.p5);
+    };
+    return MenuModeDraw;
+  })();
+  camera_input = function(key, map) {
+    console.log(key.code);
+    if (key.code === 97) {
+      return map.move_camera(1, 0);
+    } else if (key.code === 100) {
+      return map.move_camera(-1, 0);
+    } else if (key.code === 115) {
+      return map.move_camera(0, -1);
+    } else if (key.code === 119) {
+      return map.move_camera(0, 1);
+    }
+  };
   Units = (function() {
     function Units() {
       this.units = [];
@@ -91,6 +122,57 @@
     };
     return Body;
   })();
+  human_body = function() {
+    var parts;
+    parts = [];
+    parts.push(new Part("head"));
+    parts.push(new Part("arm"));
+    parts.push(new Part("leg"));
+    parts.push(new Part("leg"));
+    parts.push(new Part("arm"));
+    parts.push(new Torso());
+    return parts;
+  };
+  Map = (function() {
+    function Map(width, height) {
+      var h;
+      this.camera_x = 0;
+      this.camera_y = 0;
+      this.map = new Array(height);
+      for (h = 0; 0 <= height ? h <= height : h >= height; 0 <= height ? h++ : h--) {
+        if (h < height) {
+          this.map[h] = new Array(width);
+        }
+      }
+    }
+    Map.prototype.generate = function() {
+      var h, w, _ref, _results;
+      _results = [];
+      for (h = 0, _ref = this.map.length; 0 <= _ref ? h <= _ref : h >= _ref; 0 <= _ref ? h++ : h--) {
+        if (h < this.map.length) {
+          _results.push((function() {
+            var _ref2, _results2;
+            _results2 = [];
+            for (w = 0, _ref2 = this.map[h].length; 0 <= _ref2 ? w <= _ref2 : w >= _ref2; 0 <= _ref2 ? w++ : w--) {
+              if (w < this.map[h].length) {
+                _results2.push((Math.random() * 10) > 5 ? this.map[h][w] = 1 : this.map[h][w] = 0);
+              }
+            }
+            return _results2;
+          }).call(this));
+        }
+      }
+      return _results;
+    };
+    Map.prototype.result = function() {
+      return this.map;
+    };
+    Map.prototype.move_camera = function(x, y) {
+      this.camera_x += x;
+      return this.camera_y += y;
+    };
+    return Map;
+  })();
   Message = (function() {
     function Message() {
       this.msg = [];
@@ -106,26 +188,50 @@
     };
     return Message;
   })();
-  message_draw = function(p5, msg) {
-    return p5.text(msg, 0, 595);
-  };
-  human_body = function() {
-    var parts;
-    parts = [];
-    parts.push(new Part("head"));
-    parts.push(new Part("hand"));
-    parts.push(new Part("foot"));
-    parts.push(new Part("foot"));
-    parts.push(new Part("hand"));
-    parts.push(new Part("heart"));
-    return parts;
-  };
   Part = (function() {
     function Part(name) {
       this.name = name;
-      this.status = 0;
+      this.subparts = [];
     }
     return Part;
+  })();
+  Subpart = (function() {
+    function Subpart(name, type) {
+      this.name = name;
+      this.type = type;
+      this.damage = 0;
+    }
+    return Subpart;
+  })();
+  Torso = (function() {
+    __extends(Torso, Part);
+    function Torso() {
+      Torso.__super__.constructor.call(this, "Torso");
+      this.subparts.push(new Subpart("heart", 1));
+      this.subparts.push(new Subpart("left_lung", 2));
+      this.subparts.push(new Subpart("right_lung", 2));
+    }
+    Torso.prototype.lung_damage = function(choice) {
+      this.subparts[choice].damage = 1;
+      if (this.subparts[1].damage === 1 && this.subparts[2].damage === 1) {
+        return true;
+      }
+    };
+    Torso.prototype.interact = function() {
+      var random;
+      random = Math.round(Math.random() * this.subparts.length - 1);
+      if (this.subparts[random].type === 2) {
+        if (this.lung_damage(random)) {
+          return 2;
+        }
+      } else if (this.subparts[random].type === 1) {
+        this.subparts[random].damage = 1;
+        return 1;
+      } else {
+        return 0;
+      }
+    };
+    return Torso;
   })();
   Unit = (function() {
     function Unit(x, y, name, type) {
@@ -224,6 +330,12 @@
     };
     return mapDraw;
   })();
+  message_draw = function(p5, msg) {
+    return p5.text(msg, 0, 595);
+  };
+  titleDraw = function(p5) {
+    return p5.text("The City", 400, 300);
+  };
   unitDraw = (function() {
     function unitDraw(p5, units, map) {
       this.p5 = p5;
@@ -249,48 +361,21 @@
     };
     return unitDraw;
   })();
-  camera_input = function(key, map) {
-    console.log(key.code);
-    if (key.code === 97) {
-      return map.move_camera(1, 0);
-    } else if (key.code === 100) {
-      return map.move_camera(-1, 0);
-    } else if (key.code === 115) {
-      return map.move_camera(0, -1);
-    } else if (key.code === 119) {
-      return map.move_camera(0, 1);
-    }
-  };
   menu = function(p5) {
     p5.setup = function() {
       p5.size(800, 600);
       p5.frameRate(50);
       p5.background(0);
-      this.map = new Map(100, 100);
-      this.map.generate();
-      this.units = new Units();
-      this.units.units.push(new Unit(10, 10, "Miya", 1));
-      this.units.units.push(new Unit(10, 20, "John", 1));
-      this.units.units[1].hostility = 1;
-      this.units.units[0].target = this.units.units[1];
-      this.unit_draw = new unitDraw(p5, this.units, this.map);
-      return this.message = new Message();
+      this.mode = 1;
+      this.draw_mode = new ModeDraw(p5);
+      return this.logic_mode = new Mode();
     };
-    p5.keyPressed = function() {
-      return camera_input(p5.key, this.map);
-    };
+    p5.keyPressed = function() {};
     p5.logic = function() {
-      this.units.move();
-      this.message.update(this.units.units);
-      return this.units.clean();
+      this.logic_mode.act(this.mode);
+      return this.draw_mode.draw(this.mode);
     };
     return p5.draw = function() {
-      var map_draw;
-      p5.background(0);
-      map_draw = new mapDraw(100, 100);
-      map_draw.draw(p5, this.map);
-      this.unit_draw.draw();
-      message_draw(p5, this.message.msg[this.message.msg.length - 1]);
       return p5.logic();
     };
   };
@@ -299,4 +384,23 @@
     canvas = document.getElementById("processing");
     return processing = new Processing(canvas, menu);
   });
+  Mode = (function() {
+    function Mode() {
+      this.modes = list();
+    }
+    Mode.prototype.act = function(n) {
+      return this.modes[n].act();
+    };
+    return Mode;
+  })();
+  ModeDraw = (function() {
+    function ModeDraw(p5) {
+      this.p5 = p5;
+      this.modes = listDraw(this.p5);
+    }
+    ModeDraw.prototype.draw = function(n) {
+      return this.modes[n].draw();
+    };
+    return ModeDraw;
+  })();
 }).call(this);
